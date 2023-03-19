cbdb99d3744b08cd99204f0cce5b3b3612aaf8b8
Richard Tichý <tichy@artify.zone>
2023-03-19 16:38:55 +0100

    Implement solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
+enum class InnerRes {
+    EVEN, ODD, NOT_COUNTED
+}
+
+// solution for https://codeforces.com/contest/1807/problem/C
 fun main() {
-    println("Hello world!")
+    (1..readln().toInt()).forEach { _ ->
+        readln().toInt()
+        val s = readln().trim()
+        var answer = true
+        for (i in (0x61..0x7a)) {
+            if(!answer) break
+
+            val c = i.toChar()
+
+            s.mapIndexed { idx, it -> if(it == c) {
+                if(idx % 2 == 0) InnerRes.EVEN else InnerRes.ODD
+            } else InnerRes.NOT_COUNTED }
+                .reduce { prev, it ->
+                    if(it == InnerRes.NOT_COUNTED) prev
+                    else if(prev == InnerRes.NOT_COUNTED) it
+                    else if(prev == it) prev
+                    else {
+                        answer = false
+                        return@reduce prev
+                    }
+                }
+        }
+
+        println(if(answer) "YES" else "NO")
+    }
 }
